2d4bb71bb51c55772044132302db3fdc17572961
Tiago Felipe <tiagopai@gmail.com>
2016-12-22 12:47:30 -0200

    Create WebConfig.java

---
+++ src/main/java/br/com/tf/desafio/main/WebConfig.java
@@ -0,0 +1,12 @@
+package br.com.tf.desafio.main;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
