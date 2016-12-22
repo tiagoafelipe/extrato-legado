774c9892fbb659fa50716e448f48d36db4fff828
Tiago Felipe <tiagopai@gmail.com>
2016-12-22 12:55:49 -0200

    Update app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,15 @@ angular
 	.module('appDesafioCielo', [
 	        'appDesafioCielo.app.extrato.controller',
             'appDesafioCielo.app.extrato.service'])
-    .constant("config", {
+   	 .constant("config", {
 	  		appName: "App Desafio Cielo",
 	  		appVersion: "1.0",
 	  		apiUrl: "https://desafio-cielo-extrato.herokuapp.com"
   });
+angular.config(['$httpProvider', function ($httpProvider) {
+  //Reset headers to avoid OPTIONS request (aka preflight)
+  $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
+}]);
