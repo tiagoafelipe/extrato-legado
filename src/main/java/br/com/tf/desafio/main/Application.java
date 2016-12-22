8741bfe667d58d04269c4226373eb9ffbbf3487e
Tiago Felipe <tiagopai@gmail.com>
2016-12-22 12:30:58 -0200

    Configuracao do Cors

--- src/main/java/br/com/tf/desafio/main/Application.java
+++ src/main/java/br/com/tf/desafio/main/Application.java
@@ -2,13 +2,29 @@ package br.com.tf.desafio.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"br.com.tf.desafio"})
-public class Application   {
-
+@ComponentScan(basePackages = {"br.com.tf.desafio" })
+public class Application  {
+ 
+	
+	   
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/extrato").allowedOrigins("http://desafio-cielo-extrato.herokuapp.com/");
+            }
+        };
     }
 }
