46d3f7fd205b4fc71cd7f5e0da07b89bcc9695e8
Tiago Felipe <tiagopai@gmail.com>
2016-12-22 12:41:38 -0200

    Update ExtratoController.java

--- src/main/java/br/com/tf/desafio/api/lancamentoconta/ExtratoController.java
+++ src/main/java/br/com/tf/desafio/api/lancamentoconta/ExtratoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tf.desafio.api.lancamentoconta.domain.ExtratoLancamento;
 
+@CrossOrigin(origins = "https://desafio-cielo-extrato.herokuapp.com", maxAge = 3600)
 @RestController
 public class ExtratoController {
 
@@ -21,6 +22,7 @@ public class ExtratoController {
 	private LancamentoContaService service;
 	
 	private final Logger logger = LoggerFactory.getLogger(ExtratoController.class);
+	
 	
 	@RequestMapping(value = "/extrato", method = RequestMethod.GET)
 	public @ResponseBody ResponseEntity<List<ExtratoLancamento>> listarExtratoLancamento() {
