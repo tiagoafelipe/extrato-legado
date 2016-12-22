ea723d260f870a2dad0f1f716fcf0b0ed3f22ef1
Tiago Felipe <tiagopai@gmail.com>
2016-12-22 17:55:21 -0200

    Update ExtratoController.java

--- src/main/java/br/com/tf/desafio/api/lancamentoconta/ExtratoController.java
+++ src/main/java/br/com/tf/desafio/api/lancamentoconta/ExtratoController.java
@@ -1,5 +1,6 @@
 package br.com.tf.desafio.api.lancamentoconta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,33 +8,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.tf.desafio.api.lancamentoconta.builder.ExtratoLancamentoBuilder;
 import br.com.tf.desafio.api.lancamentoconta.domain.ExtratoLancamento;
-
-import com.knappsack.swagger4springweb.controller.ApiDocumentationController;
 
 
 @RestController
-@RequestMapping("/extrato")
-public class ExtratoController extends ApiDocumentationController{
-
-    public ExtratoController() {
-	    setBasePath("https://desafio-cielo-extrato.herokuapp.com");
-	    setBaseControllerPackage("br.com.tf.desafio.api");
-	    setBaseModelPackage("br.com.tf.desafio");
-	    setApiVersion("v1");
-    }
+public class ExtratoController {
 
 	@Autowired
 	private LancamentoContaService service;
 	
 	private final Logger logger = LoggerFactory.getLogger(ExtratoController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+    @GetMapping("/extrato")
 	public @ResponseBody ResponseEntity<List<ExtratoLancamento>> listarExtratoLancamento() {
 		
 		try {
@@ -46,7 +38,13 @@ public class ExtratoController extends ApiDocumentationController{
 			
 			logger.error(e.getMessage());
 			
-			return new ResponseEntity<List<ExtratoLancamento>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			//return new ResponseEntity<List<ExtratoLancamento>>(HttpStatus.INTERNAL_SERVER_ERROR );
+			
+			
+			List<ExtratoLancamento>  extrato = new ArrayList<ExtratoLancamento>();
+			extrato.add(new ExtratoLancamentoBuilder().nomeTipoOperacao(e.getMessage()).build());
+			
+			return new ResponseEntity<List<ExtratoLancamento>>(extrato,HttpStatus.OK);
 		}
 	}
 	
